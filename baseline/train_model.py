964e623a0cb163ca2f18f499bf3532bd679521ea
Sean2418 <sean20030418@gmail.com>
2026-03-05 23:52:32 -0500

    Fixed dir path

--- baseline/train_model.py
+++ baseline/train_model.py
@@ -2,6 +2,6 @@ from omegaconf import OmegaConf
 from rnn_trainer import BrainToTextDecoder_Trainer
 
 if __name__ == "__main__":
-    args = OmegaConf.load("rnn_args_sampled.yaml")
+    args = OmegaConf.load("rnn_args.yaml")
     trainer = BrainToTextDecoder_Trainer(args)
     metrics = trainer.train()
